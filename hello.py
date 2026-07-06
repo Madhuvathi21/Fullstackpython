d5e3947871de94862bcd7fbb5d363fea7e6d6bd7
Madhuvathi <madhuvathi21@gmail.com>
2026-07-06 14:57:01 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+username="mohan"
+followers=10000
+verified=True
+rating=4.5
+print(username)
+print(followers)
+
+username=input("Enter your name: ")
+followers=int(input("Enter your followers count: "))
+verified=input("Are you verified? (True/False): ") == "True"
+rating=float(input("Enter your rating: "))
+
+print("username:", username)
+print("followers:", followers)
+print("verified:", verified)
+print("rating:", rating)
